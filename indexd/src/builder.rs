9569d77414f329d127440e2260a36d98b52fce68
Alright <mzlot555@gmail.com>
2026-03-05 15:31:11 -0500

    remove quic specific variant from BuilderError;
    fix imports;

--- indexd/src/builder.rs
+++ indexd/src/builder.rs
@@ -12,7 +12,7 @@ use url::Url;
 
 use crate::app_client::{self, Client, RegisterAppRequest};
 use crate::object_encryption::derive;
-use crate::{SDK, quic};
+use crate::SDK;
 
 /// The initial state of the SDK builder, before connecting to the indexd service.
 pub struct DisconnectedState;
@@ -48,8 +48,8 @@ pub enum BuilderError {
     #[error("client error: {0}")]
     Client(#[from] app_client::Error),
 
-    #[error("quic error: {0}")]
-    QUIC(#[from] quic::ConnectError),
+    #[error("transport error: {0}")]
+    Transport(String),
 
     #[error("mnemonic error: {0}")]
     Mnemonic(#[from] seed::SeedError),
